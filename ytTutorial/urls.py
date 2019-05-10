0eb73a503b3115b144fd0c4d6ad50388802a254a
rcampello <rafael.campello@hotmail.com>
2019-05-10 20:17:05 -0300

    2h

--- ytTutorial/urls.py
+++ ytTutorial/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, page1_view, about_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path("", home_view, name = "home"),
     path("page1/", page1_view),
     path("about/", about_view),
+    path("product/", product_detail_view),
     path('admin/', admin.site.urls),
 ]
