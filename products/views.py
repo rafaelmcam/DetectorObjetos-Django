b75f1d16d817359fed8b39eb034d05c7f1fc78ef
rcampello <rafael.campello@hotmail.com>
2019-05-11 11:30:46 -0300

    2h 25min

--- products/views.py
+++ products/views.py
@@ -5,16 +5,27 @@ from .forms import ProductForm
 
 # Create your views here.
 
-def product_create_view(request, *args, **kwargs):
-    form = ProductForm(request.POST or None)
 
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-    context = {
-        "form": form
-    }
+def product_create_view(request, *args, **kwargs):
+    #print(request.GET)
+    #print(request.POST)
+    #title = request.POST["title"]
+    if request.method == "POST":
+        my_new_title = request.POST["title"]
+        print(my_new_title)
+    context = {}
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request, *args, **kwargs):
+#     form = ProductForm(request.POST or None)
+
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request, *args, **kwargs):
